630542fa0d8053e8193e263590aa23589ec6b0a0
Adam Zdebel <zdebcio97@gmail.com>
2020-09-19 17:21:28 +0200

    Remove unnecessary  comments

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,195 +1,17 @@
-import React from "react";
+import React from 'react';
 
-import CardsListToChoose from "./CardsListToChoose";
-import ActualUserCards from "./ActualUserCards";
-import SelectedCards from "./SelectedCards";
-import BoardWithPositions from "./BoardWithPositions";
-import FinalResults from "./FinalResults";
-import NavigationBar from "./NavigatonBar";
+import CardsListToChoose from './CardsListToChoose';
+import ActualUserCards from './ActualUserCards';
+import SelectedCards from './SelectedCards';
+import BoardWithPositions from './BoardWithPositions';
+import FinalResults from './FinalResults';
+import NavigationBar from './NavigatonBar';
 
-import "../styles/Game.css";
+import '../styles/Game.css';
 
 class Game extends React.Component {
   state = {
-    users: [
-      // {
-      //   name: "ggg1",
-      //   cards: [
-      //     {
-      //       id: 10,
-      //       name: "G. Higuaín",
-      //       ovr: 88,
-      //       face: "images/faces/gonzalo-higuain.png",
-      //       country: "images/countries/argentina.png",
-      //       club: "images/clubs/chelsea.png",
-      //       hisPosition: "cf",
-      //       position: {
-      //         gk: false,
-      //         lb: false,
-      //         rb: false,
-      //         cb: false,
-      //         dmf: false,
-      //         cmf: false,
-      //         amf: false,
-      //         lmf: false,
-      //         rmf: true,
-      //         lwf: false,
-      //         rwf: false,
-      //         ss: true,
-      //         cf: true
-      //       },
-      //       cardSelected: false,
-      //       isActive: false,
-      //       isSelected: false
-      //     },
-      //     {
-      //       id: 11,
-      //       name: "L. Modrić",
-      //       ovr: 91,
-      //       face: "images/faces/luka-modric.png",
-      //       country: "images/countries/croatia.png",
-      //       club: "images/clubs/real-madrid.png",
-      //       hisPosition: "cmf",
-      //       position: {
-      //         gk: false,
-      //         lb: false,
-      //         rb: false,
-      //         cb: false,
-      //         dmf: false,
-      //         cmf: true,
-      //         amf: true,
-      //         lmf: true,
-      //         rmf: true,
-      //         lwf: false,
-      //         rwf: false,
-      //         ss: false,
-      //         cf: false
-      //       },
-      //       cardSelected: false,
-      //       isActive: false,
-      //       isSelected: false
-      //     }
-      //   ]
-      // },
-      // {
-      //   name: "ggg2",
-      //   cards: [
-      //     {
-      //       id: 12,
-      //       name: "D. de Gea",
-      //       ovr: 91,
-      //       face: "images/faces/david-de-gea.png",
-      //       country: "images/countries/spain.png",
-      //       club: "images/clubs/manchester-united.png",
-      //       hisPosition: "gk",
-      //       position: {
-      //         gk: true,
-      //         lb: false,
-      //         rb: false,
-      //         cb: false,
-      //         dmf: false,
-      //         cmf: false,
-      //         amf: false,
-      //         lmf: false,
-      //         rmf: false,
-      //         lwf: false,
-      //         rwf: false,
-      //         ss: false,
-      //         cf: false
-      //       },
-      //       cardSelected: false,
-      //       isActive: false,
-      //       isSelected: false
-      //     },
-      //     {
-      //       id: 13,
-      //       name: "L. Suarez",
-      //       ovr: 90,
-      //       face: "images/faces/luis-suarez.png",
-      //       country: "images/countries/uruguay.png",
-      //       club: "images/clubs/barcelona.png",
-      //       hisPosition: "cf",
-      //       position: {
-      //         gk: false,
-      //         lb: false,
-      //         rb: false,
-      //         cb: false,
-      //         dmf: false,
-      //         cmf: false,
-      //         amf: false,
-      //         lmf: false,
-      //         rmf: false,
-      //         lwf: true,
-      //         rwf: true,
-      //         ss: true,
-      //         cf: true
-      //       },
-      //       cardSelected: false,
-      //       isActive: false,
-      //       isSelected: false
-      //     }
-      //   ]
-      // },
-      // {
-      //   name: "ggg3",
-      //   cards: [
-      //     {
-      //       id: 14,
-      //       name: "R. Nainggolan",
-      //       ovr: 85,
-      //       face: "images/faces/radja-nainggolan.png",
-      //       country: "images/countries/belgium.png",
-      //       club: "images/clubs/internazionale.png",
-      //       hisPosition: "cmf",
-      //       position: {
-      //         gk: false,
-      //         lb: false,
-      //         rb: false,
-      //         cb: false,
-      //         dmf: true,
-      //         cmf: true,
-      //         amf: true,
-      //         lmf: false,
-      //         rmf: false,
-      //         lwf: false,
-      //         rwf: false,
-      //         ss: false,
-      //         cf: false
-      //       },
-      //       cardSelected: false,
-      //       isActive: false,
-      //       isSelected: false
-      //     },
-      //     {
-      //       id: 15,
-      //       name: "R. Lewandowski",
-      //       ovr: 90,
-      //       face: "images/faces/robert-lewandowski.png",
-      //       country: "images/countries/poland.png",
-      //       club: "images/clubs/bayern-munchen.png",
-      //       hisPosition: "cf",
-      //       position: {
-      //         gk: false,
-      //         lb: false,
-      //         rb: false,
-      //         cb: false,
-      //         dmf: false,
-      //         cmf: false,
-      //         amf: true,
-      //         lmf: false,
-      //         rmf: false,
-      //         lwf: false,
-      //         rwf: false,
-      //         ss: true,
-      //         cf: true
-      //       },
-      //       cardSelected: false,
-      //       isActive: false,
-      //       isSelected: false
-      //     }
-      //   ]
-      // }
-    ],
+    users: [],
     players: [],
     playersFilterAndRandom: [],
     selectedPlayers: [],
@@ -199,7 +21,7 @@ class Game extends React.Component {
     isDrawStarted: false,
     areCardsOpened: false,
     isCardSelected: false,
-    numberOfCards: 0
+    numberOfCards: 0,
   };
 
   componentDidMount() {
@@ -207,41 +29,41 @@ class Game extends React.Component {
   }
 
   waitForData = () => {
-    fetch("data/players.json")
-      .then(response => {
+    fetch('data/players.json')
+      .then((response) => {
         if (response.ok) {
           return response;
         }
         throw Error(response.status);
       })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         this.setState({
           players: data,
-          isDataLoaded: true
+          isDataLoaded: true,
         });
       })
-      .catch(err => console.log(err + ". Błąd podczas pobierania danych"));
+      .catch((err) => console.log(err + '. Błąd podczas pobierania danych'));
 
     const users = [];
 
     for (let i = 0; i < this.props.users; i++) {
       users.push({
         name: `Player ${i + 1}`,
-        cards: []
+        cards: [],
       });
     }
 
     this.setState({
-      users
+      users,
     });
   };
 
-  handleFilterByPositionClick = position => {
+  handleFilterByPositionClick = (position) => {
     if (!this.state.isDrawStarted) {
       const playersFilterAndRandom = [];
       const playersFilter = this.state.players.filter(
-        player => player.position[position] && !player.cardSelected
+        (player) => player.position[position] && !player.cardSelected
       );
       while (playersFilter.length > 0) {
         const index = Math.floor(Math.random() * playersFilter.length);
@@ -250,12 +72,12 @@ class Game extends React.Component {
       }
       this.setState({
         playersFilterAndRandom,
-        isDrawStarted: true
+        isDrawStarted: true,
       });
     }
   };
 
-  handleTakeCardToOpenClick = id => {
+  handleTakeCardToOpenClick = (id) => {
     const playersFilterAndRandom = [...this.state.playersFilterAndRandom];
     let numberOfCards = 0;
     if (this.state.playersFilterAndRandom.length >= 5) {
@@ -265,9 +87,9 @@ class Game extends React.Component {
     }
 
     const activeNumber = playersFilterAndRandom.filter(
-      player => player.isActive
+      (player) => player.isActive
     ).length;
-    playersFilterAndRandom.forEach(player => {
+    playersFilterAndRandom.forEach((player) => {
       if (
         player.id === id &&
         !player.isActive &&
@@ -279,28 +101,28 @@ class Game extends React.Component {
       }
     });
     const selectedPlayers = playersFilterAndRandom.filter(
-      player => player.isActive
+      (player) => player.isActive
     );
 
     this.setState({
       playersFilterAndRandom,
       selectedPlayers,
-      numberOfCards
+      numberOfCards,
     });
   };
 
   handleOpenCardsClick = () => {
     if (this.state.selectedPlayers.length === this.state.numberOfCards) {
       this.setState({
-        areCardsOpened: true
+        areCardsOpened: true,
       });
     }
   };
 
-  handleIsCardSelectedClick = id => {
+  handleIsCardSelectedClick = (id) => {
     const selectedPlayers = [...this.state.selectedPlayers];
     if (this.state.areCardsOpened) {
-      selectedPlayers.forEach(player => {
+      selectedPlayers.forEach((player) => {
         if (player.id !== id) {
           player.isSelected = false;
         } else if (player.id === id) {
@@ -310,7 +132,7 @@ class Game extends React.Component {
     }
 
     this.setState({
-      selectedPlayers
+      selectedPlayers,
     });
   };
 
@@ -320,7 +142,7 @@ class Game extends React.Component {
     const users = this.state.users;
 
     let index = 0;
-    const playerChoose = selectedPlayers.filter(player => player.isSelected);
+    const playerChoose = selectedPlayers.filter((player) => player.isSelected);
     if (playerChoose.length === 1) {
       players.forEach((player, id) => {
         if (player.id === playerChoose[0].id) {
@@ -334,7 +156,7 @@ class Game extends React.Component {
 
       this.setState({
         users,
-        isCardSelected: true
+        isCardSelected: true,
       });
     }
   };
@@ -343,7 +165,7 @@ class Game extends React.Component {
     const playersFilterAndRandom = [];
     const selectedPlayers = [];
     const players = [...this.state.players];
-    players.forEach(player => {
+    players.forEach((player) => {
       player.cardSelected = false;
       player.isActive = false;
       player.isSelected = false;
@@ -370,7 +192,7 @@ class Game extends React.Component {
       selectedPlayers,
       areCardsOpened: false,
       isCardSelected: false,
-      isDrawStarted: false
+      isDrawStarted: false,
     });
   };
 

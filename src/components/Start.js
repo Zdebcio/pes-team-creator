a9c5c548a7619586f7d73e6cd906fdf9a1a934a5
Adam Zdebel <zdebcio97@gmail.com>
2020-09-19 17:19:25 +0200

    Change language in Start.js

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/Start.css";
+import '../styles/Start.css';
 
-const Start = props => {
+const Start = (props) => {
   return (
     <div className="start">
       <h1>PES team creator</h1>
-      <h2>Players:</h2>
+      <h2>Ilość graczy:</h2>
       <div className="users">
-        <button id="-" onClick={() => props.changeNumberOf("-", "users")}>
+        <button id="-" onClick={() => props.changeNumberOf('-', 'users')}>
           <i className="far fa-minus-square" />
         </button>
         <span>{props.users}</span>
-        <button id="+" onClick={() => props.changeNumberOf("+", "users")}>
+        <button id="+" onClick={() => props.changeNumberOf('+', 'users')}>
           <i className="far fa-plus-square" />
         </button>
       </div>
-      <h2>Footballers:</h2>
+      <h2>Ilość zawodników:</h2>
       <div className="players">
-        <button id="-" onClick={() => props.changeNumberOf("-", "players")}>
+        <button id="-" onClick={() => props.changeNumberOf('-', 'players')}>
           <i className="far fa-minus-square" />
         </button>
         <span>{props.numberOfPlayers}</span>
-        <button id="+" onClick={() => props.changeNumberOf("+", "players")}>
+        <button id="+" onClick={() => props.changeNumberOf('+', 'players')}>
           <i className="far fa-plus-square" />
         </button>
       </div>
